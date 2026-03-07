e392f95bca459906e9bac068422d14eaf791ebce
aditya-xq <adityavivek1998@gmail.com>
2026-03-08 03:19:40 +0530

    feat: add nav icons

---
+++ src/lib/components/icons/MediumIcon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { siMedium } from "simple-icons"
+
+  interface Props {
+    size?: number
+  }
+
+  let { size = 17 }: Props = $props()
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 24 24"
+  fill="currentColor"
+  aria-hidden="true"
+>
+  <path d={siMedium.path}></path>
+</svg>
---
+++ src/lib/components/icons/YoutubeIcon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { siYoutube } from "simple-icons"
+
+  interface Props {
+    size?: number
+  }
+
+  let { size = 17 }: Props = $props()
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 24 24"
+  fill="currentColor"
+  aria-hidden="true"
+>
+  <path d={siYoutube.path}></path>
+</svg>
